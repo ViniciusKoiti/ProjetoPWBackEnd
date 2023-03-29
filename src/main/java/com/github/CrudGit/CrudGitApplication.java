314ce47d629e444a954949a2cf9c53aa5096431d
Vinicius Koiti <Vinicius Koiti>
2023-03-28 22:36:50 -0300

    Iniciando Projeto

---
+++ src/main/java/com/github/CrudGit/CrudGitApplication.java
@@ -0,0 +1,13 @@
+package com.github.CrudGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/CrudGit/CrudGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.CrudGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
