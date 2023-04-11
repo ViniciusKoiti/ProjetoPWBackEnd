b4039d13033ee048cd097ab292090fad25cf2444
Vinicius Koiti <viniciusnakahara@gmail.com>
2023-04-11 17:57:14 -0300

    Alteração para salvar os User

--- src/main/java/com/github/CrudGit/Controllers/CommitController.java
+++ src/main/java/com/github/CrudGit/Controllers/CommitController.java
@@ -3,7 +3,6 @@ package com.github.CrudGit.Controllers;
 import com.github.CrudGit.Controllers.CommitResponse.CommitResponse;
 import com.github.CrudGit.Services.CommitService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin
 @RequestMapping("/commit")
-@CrossOrigin
 public class CommitController {
     @Autowired
     private final CommitService commitService;
--- src/main/java/com/github/CrudGit/Controllers/CommitResponse/UtilResponse/UtilResponse.java
+++ src/main/java/com/github/CrudGit/Controllers/CommitResponse/UtilResponse/UtilResponse.java
@@ -4,5 +4,5 @@ public class UtilResponse {
 
     public static final String MSG_RESUMO_COMMIT = "Foram realizados commits %s no dia %d";
 
-
+    public static final String URL_GITHUB_REPOSITORY = "https://github.com/%d/%s";
 }
